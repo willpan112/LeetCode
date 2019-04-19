57241a0eb30c629799d074b0dce422a37d80e653
willpan <873201921@qq.com>
2019-04-19 11:47:12 +0800

    Add Two Sum solution

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # LeetCode
-[![GitHub license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/willpan112/LeetCode/blob/master/LICENSE.md)
+[![GitHub license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/willpan112/Leetcode/blob/master/LICENSE)
 
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
+|1|[Two Sum](https://oj.leetcode.com/problems/two-sum/)| [JavaScript](./solutiongs/twoSum.js)|Easy|
---
+++ solutions/towSum.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: willpan 
+ * @Date: 2019-04-19 11:41:05 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2019-04-19 11:45:48
+ */
+
+/*************** 
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * 
+ * Example:
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9
+ * Because nums[0] + nums[1] = 2 + 7 = 9
+ * return [0, 1]
+*******************/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map()
+    for (let i = 0, len = nums.length; i < len; i++) {
+        if (map.has(nums[i])) {
+            return [map.get(nums[i]), i]
+        } else {
+            map.set((target - nums[i]), i)
+        }
+    }
+};
